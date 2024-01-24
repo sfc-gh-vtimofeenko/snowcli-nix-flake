9987c50380fe69b41a39dd957605b1ddd2e930b4
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-01-24 15:59:57 -0800

    feat: add home-manager module

--- flake.nix
+++ flake.nix
@@ -39,92 +39,99 @@
     };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+  outputs = inputs@{ flake-parts, self, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { flake-parts-lib, ... }:
+      {
+        systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-      imports = [
-        inputs.flake-parts.flakeModules.easyOverlay
+        imports = [
+          inputs.flake-parts.flakeModules.easyOverlay
 
-        inputs.devshell.flakeModule
-        inputs.pre-commit-hooks-nix.flakeModule
-        inputs.treefmt-nix.flakeModule
-      ];
+          inputs.devshell.flakeModule
+          inputs.pre-commit-hooks-nix.flakeModule
+          inputs.treefmt-nix.flakeModule
+        ];
 
-      perSystem =
-        { config
-        , pkgs
-          /* These inputs are unused in the template, but might be useful later */
-          # , self'
-          # , inputs'
-          # , system
-        , ...
-        }: {
-          packages =
-            let
-              mkSnowpark = { src, version }: pkgs.callPackage ./packages/snowflake-connector-python/mkSnowflakeConnectorPython.nix { inherit (pkgs) python3; inherit src version; };
-              mkSnowcli = { src, version, snowflakeConnectorPkg }: pkgs.callPackage ./packages/snowcli/mkSnowcli.nix { inherit (pkgs) python3 lib; inherit src version snowflakeConnectorPkg; };
+        perSystem =
+          { config
+          , pkgs
+            /* These inputs are unused in the template, but might be useful later */
+            # , self'
+            # , inputs'
+            # , system
+          , ...
+          }: {
+            packages =
+              let
+                mkSnowpark = { src, version }: pkgs.callPackage ./packages/snowflake-connector-python/mkSnowflakeConnectorPython.nix { inherit (pkgs) python3; inherit src version; };
+                mkSnowcli = { src, version, snowflakeConnectorPkg }: pkgs.callPackage ./packages/snowcli/mkSnowcli.nix { inherit (pkgs) python3 lib; inherit src version snowflakeConnectorPkg; };
 
-              snowpark-for-snowcli-1x = mkSnowpark { src = inputs.snowflake-connector-python-1x; version = "3.2.0"; };
-              snowpark-for-snowcli-2x = mkSnowpark { src = inputs.snowflake-connector-python-2x; version = "3.6.0"; };
-            in
-            {
-              inherit snowpark-for-snowcli-1x snowpark-for-snowcli-2x;
+                snowpark-for-snowcli-1x = mkSnowpark { src = inputs.snowflake-connector-python-1x; version = "3.2.0"; };
+                snowpark-for-snowcli-2x = mkSnowpark { src = inputs.snowflake-connector-python-2x; version = "3.6.0"; };
+                snowcli-1x = mkSnowcli {
+                  src = inputs.snowcli-src-1x;
+                  version = "1.2.4";
+                  snowflakeConnectorPkg = snowpark-for-snowcli-1x;
+                };
+                snowcli-2x = mkSnowcli {
+                  src = inputs.snowcli-src-2x;
+                  version = "2.0.0-dev";
+                  snowflakeConnectorPkg = snowpark-for-snowcli-2x;
+                };
+              in
+              {
+                inherit snowpark-for-snowcli-1x snowpark-for-snowcli-2x snowcli-2x snowcli-1x;
 
-              snowcli-1x = mkSnowcli {
-                src = inputs.snowcli-src-1x;
-                version = "1.2.4";
-                snowflakeConnectorPkg = snowpark-for-snowcli-1x;
+                default = snowcli-2x;
               };
-              snowcli-2x = mkSnowcli {
-                src = inputs.snowcli-src-2x;
-                version = "2.0.0-dev";
-                snowflakeConnectorPkg = snowpark-for-snowcli-2x;
+            overlayAttrs = builtins.removeAttrs config.packages [ "default" ];
+
+            /* Development configuration */
+            treefmt = {
+              programs = {
+                nixpkgs-fmt.enable = true;
+                deadnix = {
+                  enable = true;
+                  no-lambda-arg = true;
+                  no-lambda-pattern-names = true;
+                  no-underscore = true;
+                };
+                statix.enable = true;
+              };
+              projectRootFile = "flake.nix";
+            };
+
+            pre-commit.settings = {
+              hooks = {
+                treefmt.enable = true;
+                deadnix.enable = true;
+                statix.enable = true;
+              };
+              settings = {
+                deadnix.edit = true;
+                statix = {
+                  ignore = [ ".direnv/" ];
+                  format = "stderr";
+                };
+                treefmt.package = config.treefmt.build.wrapper;
               };
             };
-          overlayAttrs = config.packages;
-          /* Development configuration */
-          treefmt = {
-            programs = {
-              nixpkgs-fmt.enable = true;
-              deadnix = {
-                enable = true;
-                no-lambda-arg = true;
-                no-lambda-pattern-names = true;
-                no-underscore = true;
-              };
-              statix.enable = true;
+
+            devShells.pre-commit = config.pre-commit.devShell;
+            devshells.default = {
+              env = [ ];
+              commands = [ ];
+              packages = [
+                config.pre-commit.settings.package
+                config.treefmt.build.wrapper
+              ];
             };
-            projectRootFile = "flake.nix";
           };
 
-          pre-commit.settings = {
-            hooks = {
-              treefmt.enable = true;
-              deadnix.enable = true;
-              statix.enable = true;
-            };
-            settings = {
-              deadnix.edit = true;
-              statix = {
-                ignore = [ ".direnv/" ];
-                format = "stderr";
-              };
-              treefmt.package = config.treefmt.build.wrapper;
-            };
-          };
+        /* Home manager module that configures snowcli */
+        flake.homeManagerModules.default = flake-parts-lib.importApply ./modules/homeManager { localFlake = self; };
 
-          devShells.pre-commit = config.pre-commit.devShell;
-          devshells.default = {
-            env = [ ];
-            commands = [ ];
-            packages = [
-              config.pre-commit.settings.package
-              config.treefmt.build.wrapper
-            ];
-          };
-        };
-
-      flake = { };
-    };
+      }
+    );
 }
---
+++ modules/homeManager/default.nix
@@ -0,0 +1,49 @@
+/* Home manager module for snowcli */
+{ localFlake }:
+{ config, pkgs, lib, ... }:
+let
+  inherit (lib) mkOption mkEnableOption types mkIf;
+  inherit (pkgs.stdenv.hostPlatform) system isDarwin;
+
+  cfg = config.programs.snowcli;
+  selfPkgs' = localFlake.packages.${system};
+  settingsFormat = pkgs.formats.toml { };
+
+  /* Depending on the platform, the config should be in a different location */
+  configFile = (if isDarwin then "Library/Application Support" else config.xdg.configHome) + "/snowflake/config.toml";
+in
+{
+  options.programs.snowcli = {
+    enable = mkEnableOption "Snowcli";
+    package = mkOption {
+      type = types.package;
+      inherit (selfPkgs') default;
+      description = lib.mdDoc "Package that provides Snowcli";
+    };
+    settings = lib.mkOption {
+      type = types.submodule {
+        freeformType = settingsFormat.type;
+      };
+      example = {
+        connections.dev = {
+          account = "account_identifier";
+          user = "username";
+          database = "some_database";
+          authenticator = "externalbrowser";
+        };
+      };
+      description = lib.mdDoc ''
+        Snowcli configuration.
+
+        *NOTE*: `connection.dev` is used by default.
+
+        See [doc](https://github.com/Snowflake-Labs/snowcli) for more information.
+      '';
+    };
+  };
+  config = mkIf cfg.enable {
+    home.packages = [ cfg.package ];
+    home.file.${configFile}.source = settingsFormat.generate "config.toml" cfg.settings;
+  };
+}
+
