0e76da6ba2a8cc8250c3372e046fb083ce661e64
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-02-05 09:40:49 -0800

    fix: correct package name in the nix store

--- packages/snowflake-connector-python/mkSnowflakeConnectorPython.nix
+++ packages/snowflake-connector-python/mkSnowflakeConnectorPython.nix
@@ -6,6 +6,7 @@ python3.pkgs.snowflake-connector-python.overrideAttrs
   (
     _finalAttrs: previousAttrs: {
       inherit src version;
+      name = "snowflake-connector-python-${version}";
       propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++
         (builtins.attrValues { inherit (python3.pkgs) sortedcontainers packaging platformdirs tomlkit keyring; });
     }
