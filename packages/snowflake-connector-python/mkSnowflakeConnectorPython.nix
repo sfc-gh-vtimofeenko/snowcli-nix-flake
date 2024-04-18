ce8f56664f0996ee0463dc3cfc7ee06dcf950143
Vladimir Timofeenko <vladimir.timofeenko@snowflake.com>
2024-04-18 09:18:11 -0700

    fix: relax snowpark python deps

--- packages/snowflake-connector-python/mkSnowflakeConnectorPython.nix
+++ packages/snowflake-connector-python/mkSnowflakeConnectorPython.nix
@@ -2,12 +2,22 @@
 , python3
 , version
 }:
-python3.pkgs.snowflake-connector-python.overrideAttrs
-  (
-    _finalAttrs: previousAttrs: {
-      inherit src version;
-      name = "snowflake-connector-python-${version}";
-      propagatedBuildInputs = previousAttrs.propagatedBuildInputs ++
-        (builtins.attrValues { inherit (python3.pkgs) sortedcontainers packaging platformdirs tomlkit keyring; });
-    }
-  )
+python3.pkgs.snowflake-connector-python.overrideAttrs (
+  _finalAttrs: previousAttrs: {
+    inherit src version;
+    name = "snowflake-connector-python-${version}";
+    propagatedBuildInputs =
+      previousAttrs.propagatedBuildInputs
+      ++ (builtins.attrValues {
+        inherit (python3.pkgs)
+          sortedcontainers
+          packaging
+          platformdirs
+          tomlkit
+          keyring
+          pythonRelaxDepsHook
+          ;
+      });
+    pythonRelaxDeps = true;
+  }
+)
